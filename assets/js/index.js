6d8dbab7a0a4a29d88777652935779cdda5a2db8
Stephen Chung <signup@stephenchung.me>
2019-02-11 17:59:55 -0500

    Add scrolling movement

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,15 @@
+import $ from 'jquery';
 import runSplash from './splash';
+// import * as Waypoints from 'waypoints';
 
 document.addEventListener("DOMContentLoaded", () => {
   runSplash();
+
+  $('.scroll-to').click((e) => {
+    const jump = $('.scroll-to').attr('href');
+    const new_position = $(jump).offset();
+    $('html, body').stop().animate({ scrollTop: new_position.top }, 500);
+    e.preventDefault();
+  });
+  
 });
